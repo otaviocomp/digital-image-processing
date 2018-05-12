c40912e7e28255faeb80eeb51d47dbb19e19ba70
otaviocomp <otavio10espsanto@gmail.com>
2018-05-12 20:35:48 +0000

    update

--- codes/homomorfico.cpp
+++ codes/homomorfico.cpp
@@ -1,173 +1,197 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <math.h>
 
 #define RADIUS 20
 
 using namespace cv;
 using namespace std;
 
-#define MAX 100.0
-
-int height;
-
-int dft_M, dft_N;
-
-float gamal = 0;
-float max_gamal = 100;
-int gamal_slider = 0;
-
-int d0_slider = 0;
-float max_d0 = 256;
-float d0 = 0;
-
-float gamah = 0;
-float max_gamah = 100;
-int gamah_slider = 0;
+	int yL, yLmax=100;
+	int yH, yHmax=100;
+	int D0, D0max=100;
+	int C0, C0max=100;
 
 
-Mat image, imageDft, padded;
+  Mat complexImage;
+  Mat padded, filter,res,filtrada;
+  Mat image,imagegray, tmp;
+  Mat_<float> realInput, zeros;
+  vector<Mat> planos;
+  // valores ideais dos tamanhos da imagem
+  // para calculo da DFT
+  int dft_M, dft_N;
+  char TrackbarName[50];
+  char key;
 
-char TrackbarName[50];
+void on_trackbar_filtro(int, void*){
 
-// troca os quadrantes da imagem da DFT como havia sido feito na 1 unidade
-void deslocaDFT(Mat& image ){
-    Mat aux, A, B, C, D;
+    int dft_M= tmp.size().height;
+    int dft_N= tmp.size().width;
 
-    // se impar recorta para nao ter imagens desiguais
-    image = image(Rect(0, 0, image.cols & -2, image.rows & -2));
-    int cx = image.cols/2;
-    int cy = image.rows/2;
+    double gH= yH/10.0;
+    double gL= yL/10.0;
+    double d0= D0/10.0;
+    // variacao 0-0.1
+    double c0= C0/1000.0;
 
-    // reorganiza os quadrantes da transformada
-    A = image(Rect(0, 0, cx, cy));
-    B = image(Rect(cx, 0, cx, cy));
-    C = image(Rect(0, cy, cx, cy));
-    D = image(Rect(cx, cy, cx, cy));
+  for(int i=0; i<dft_M; i++){
+    for(int j=0; j<dft_N; j++){
+    	double D= pow(i-dft_M/2,2)+pow(j-dft_N/2, 2);
+      tmp.at<float> (i,j) = (gH-gL)*(1-exp(-c0*D/pow(d0,2)))+gL;
+      }
+   }
 
-    // A <-> D
-    A.copyTo(aux);  D.copyTo(A);  aux.copyTo(D);
-
-    // C <-> B
-    C.copyTo(aux);  B.copyTo(C);  aux.copyTo(B);
-}
-
-void homomorfico() {
-    Mat filter = Mat(padded.size(), CV_32FC2, Scalar(0));
-    Mat aux = Mat(dft_M, dft_N, CV_32F);
-
-    for (int i = 0; i < dft_M; i++) {
-        for (int j = 0; j < dft_N; j++) {
-            float d2 = pow(i - dft_M/2.0, 2) + pow(j - dft_N/2.0, 2);
-            float valor = (gamah - gamal)*(1 - expf(- (d2/pow(d0, 2))) ) + gamal; //
-            aux.at<float> (i,j) = valor;
-        }
-    }
-
-    Mat comps[] = {aux, aux};
+    // junta os planos imaginario e real
+    Mat comps[]= {tmp, tmp};
     merge(comps, 2, filter);
+}
+// troca os quadrantes da imagem da DFT
+void deslocaDFT(Mat& image ){
+  Mat tmp, A, B, C, D;
 
-    Mat dftClone = imageDft.clone();
+  // se a imagem tiver tamanho impar, recorta a regiao para
+  // evitar cópias de tamanho desigual
+  image = image(Rect(0, 0, image.cols & -2, image.rows & -2));
+  int cx = image.cols/2;
+  int cy = image.rows/2;
 
-    mulSpectrums(dftClone,filter,dftClone,0);
+  // reorganiza os quadrantes da transformada
+  // A B   ->  D C
+  // C D       B A
+  A = image(Rect(0, 0, cx, cy));
+  B = image(Rect(cx, 0, cx, cy));
+  C = image(Rect(0, cy, cx, cy));
+  D = image(Rect(cx, cy, cx, cy));
 
-    deslocaDFT(dftClone);
+  // A <-> D
+  A.copyTo(tmp);  D.copyTo(A);  tmp.copyTo(D);
 
-    idft(dftClone, dftClone);
-
-    vector<Mat> planos;
-
-    split (dftClone, planos);
-
-    normalize(planos[0], planos[0], 0, 1, CV_MINMAX);
-
-    char bufferd0[20], bufferyh[20], bufferyl[20];
-    sprintf(bufferd0, "D0: %f", d0);
-    sprintf(bufferyh, "Alta Frequencia: %f", gamah);
-    sprintf(bufferyl, "Baixa Frequencia: %f", gamal);
-    putText(planos[0], bufferd0,cv::Point(0,10), CV_FONT_HERSHEY_SIMPLEX, 0.35, cv::Scalar(255),1,8,false);
-    putText(planos[0], bufferyh,cv::Point(0,20), CV_FONT_HERSHEY_SIMPLEX, 0.35, cv::Scalar(255),1,8,false);
-    putText(planos[0], bufferyl,cv::Point(0,30), CV_FONT_HERSHEY_SIMPLEX, 0.35, cv::Scalar(255),1,8,false);
-    imshow("Filtro Homomorfico", planos[0]);
-    imshow("Original", image);
+  // C <-> B
+  C.copyTo(tmp);  B.copyTo(C);  tmp.copyTo(B);
 }
 
-void on_trackbar_gamal(int, void*){
-    gamal = (float) gamal_slider;
-    gamal = max_gamal*gamal/MAX;
-    homomorfico();
-}
+int main(int argc, char** argv){
 
-void on_trackbar_d0(int, void *){
-    d0 = d0_slider*max_d0/MAX;
-    homomorfico();
-}
 
-void on_trackbar_gamah(int, void*) {
-    gamah = gamah_slider*max_gamah/MAX;
-    homomorfico();
-}
+  image = imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
 
-int main(int argvc, char** argv){
-    image = imread("mi.jpg");
-    cvtColor(image, image, CV_BGR2GRAY);
+  namedWindow("com filtro", CV_WINDOW_AUTOSIZE);
 
-    height = image.size().height;
+    sprintf( TrackbarName, "yH x %d", yHmax );
+    createTrackbar( TrackbarName, "com filtro",&yH,
+    						 yHmax,
+    						 on_trackbar_filtro);
 
-    // identifica os tamanhos otimos para
-    // calculo do FFT
-    dft_M = getOptimalDFTSize(image.rows);
-    dft_N = getOptimalDFTSize(image.cols);
+    sprintf( TrackbarName, "yL x %d", yLmax );
+    createTrackbar( TrackbarName, "com filtro",&yL,
+    						 yLmax,
+    						 on_trackbar_filtro);
+
+    sprintf( TrackbarName, "D0 x %d", D0max );
+    createTrackbar( TrackbarName, "com filtro",&D0,
+    						 D0max,
+    						 on_trackbar_filtro);
+
+    sprintf( TrackbarName, "C0 x %d", D0max );
+    createTrackbar( TrackbarName, "com filtro",&C0,
+    						 C0max,
+    						 on_trackbar_filtro);
+
+
+  // identifica os tamanhos otimos para
+  // calculo do FFT
+  dft_M = getOptimalDFTSize(image.rows);
+  dft_N = getOptimalDFTSize(image.cols);
+
+  // realiza o padding da imagem
+  copyMakeBorder(image, padded, 0,
+                 dft_M - image.rows, 0,
+                 dft_N - image.cols,
+                 BORDER_CONSTANT, Scalar::all(0));
+
+  // parte imaginaria da matriz complexa (preenchida com zeros)
+  zeros = Mat_<float>::zeros(padded.size());
+
+  // prepara a matriz complexa para ser preenchida
+  complexImage = Mat(padded.size(), CV_32FC2, Scalar(0));
+
+  // a função de transferência (filtro frequencial) deve ter o
+  // mesmo tamanho e tipo da matriz complexa
+  filter = complexImage.clone();
+
+  // cria uma matriz temporária para criar as componentes real
+  // e imaginaria do filtro ideal
+  tmp = Mat(dft_M, dft_N, CV_32F);
+
+
+  while(1){
+
+    imagegray= image.clone();
+
+    imshow("original", imagegray);
 
     // realiza o padding da imagem
-    Mat_<float> zeros;
-    copyMakeBorder(image, padded, 0,
+    copyMakeBorder(imagegray, padded, 0,
                    dft_M - image.rows, 0,
                    dft_N - image.cols,
                    BORDER_CONSTANT, Scalar::all(0));
-
-    // parte imaginaria da matriz complexa (preenchida com zeros)
-    zeros = Mat_<float>::zeros(padded.size());
-
-    // prepara a matriz complexa para ser preenchida
-    imageDft = Mat(padded.size(), CV_32FC2, Scalar(0));
-
-    copyMakeBorder(image, padded, 0,
-                   dft_M - image.rows, 0,
-                   dft_N - image.cols,
-                   BORDER_CONSTANT, Scalar::all(0));
-
-    Mat_<float> realInput = Mat_<float>(padded);
-    vector<Mat> planos;
+    // limpa o array de matrizes que vao compor a
+    // imagem complexa
+    planos.clear();
+    // cria a compoente real
+    realInput = Mat_<float>(padded);
+    realInput += Scalar::all(1);
+    // calcula o log da imagem
+    log(realInput, realInput);
+    // insere as duas componentes no array de matrizes
     planos.push_back(realInput);
     planos.push_back(zeros);
-    merge(planos, imageDft);
-    dft(imageDft, imageDft);
-    deslocaDFT(imageDft);
-    namedWindow("Filtro Homomorfico", 1);
 
-    sprintf( TrackbarName, "Alta Frequencia");
-    createTrackbar( TrackbarName, "Filtro Homomorfico",
-                    &gamah_slider,
-                    MAX,
-                    on_trackbar_gamah);
+    // combina o array de matrizes em uma unica
+    // componente complexa
+    merge(planos, complexImage);
+    // calcula o dft
+    dft(complexImage, complexImage);
 
-    sprintf( TrackbarName, "Baixa Frequencia");
-    createTrackbar( TrackbarName, "Filtro Homomorfico",
-                    &gamal_slider,
-                    MAX,
-                    on_trackbar_gamal);
+    // realiza a troca de quadrantes
+    deslocaDFT(complexImage);
 
-    sprintf( TrackbarName, "D0");
-    createTrackbar( TrackbarName, "Filtro Homomorfico",
-                    &d0_slider,
-                    MAX,
-                    on_trackbar_d0 );
-    homomorfico();
-    waitKey(0);
+    // aplica o filtro frequencial
+    mulSpectrums(complexImage,filter,complexImage,0);
 
-    return 0;
+    // troca novamente os quadrantes
+    deslocaDFT(complexImage);
+
+    // calcula a DFT inversa
+    idft(complexImage, complexImage,DFT_SCALE);
+
+    // limpa o array de planos
+    planos.clear();
+
+    // separa as partes real e imaginaria da
+    // imagem filtrada
+    split(complexImage, planos);
+    // calcular expodencial
+    exp(planos[0], planos[0]);
+
+    // normaliza a parte real para exibicao
+    normalize(planos[0], planos[0], 0, 1, CV_MINMAX);
+    imshow("com filtro", planos[0]);
+    planos[0].convertTo(filtrada, CV_8UC1, 255.0);
+    imwrite("comfiltro.png",filtrada);
+
+    vector<Mat> pFiltro;
+    split(filter,pFiltro);
+    normalize(pFiltro[0], pFiltro[0], 0, 1, CV_MINMAX);
+    // converte para uchar para salvar
+    pFiltro[0].convertTo(res, CV_8UC1, 255.0);
+    imshow("filtro",res);
+    imwrite("filtro.png",res);
+
+    key = (char) waitKey(10);
+    if( key == 27 ) break; // esc pressed!
+    }
+
+  return 0;
 }
-
-
